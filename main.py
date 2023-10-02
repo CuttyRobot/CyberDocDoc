59f11adc6e89455c3a0e29729c1edb0cfe2f773d
Cutty Robot <nikkiraim@gmail.com>
2023-10-02 18:59:18 +0300

    Webhook testing. Type of Message recognition and avoiding not handling types of messages data.

--- main.py
+++ main.py
@@ -30,76 +30,54 @@ async def rocker(request: fastapi.Request):
     # Loading decoded string to json format
     json_data = json.loads(input_string_decoded)
     message = json_data
-    # Message chat id
+
+    # Message first level keys
     first_level_keys = message.keys()
+
     if "message" in first_level_keys:
+        line = int(message["message"]['chat']["id"])
+        # Message second level keys
         second_level_keys = message["message"].keys()
         if "video" in second_level_keys:
             # Message with video file
             print('Type: message with video')
+            # Not handling
+            await bot.send_message(line, "Sorry, but we are not handeling video files")
+
         elif "audio" in second_level_keys:
             # Message with audio file
             print('Type: message with audio')
+            # Not handling
+            await bot.send_message(line, "Sorry, but we are not handeling Audio files")
+
         elif "document" in second_level_keys:
             # Message with document file
             print('Type: message with document')
+            # Not handling
+            await bot.send_message(line, "Sorry, but we are not handeling Documents")
+
         elif "photo" in second_level_keys:
             # Message with image file
             print('Type: message with photo')
+            # Handling
+            await bot.send_message(line, "Photo can be processed")
+
         elif "entities" in second_level_keys:
             # Text message (can include command)
             print('Type: command message')
+            # Handling
+            await bot.send_message(line, "Command messages can be processed")
+
         else:
             print('Type: common message')
-
+            await bot.send_message(line, str(message['message']['text']))
+            print(line)
+            # Handling
 
     elif "callback_query" in first_level_keys:
         print('Type: callback')
-
-    # line = 0
-    # try:
-    #     if message["message"] is not None:
-    #         # For messages
-    #         print("Update_id:", message["update_id"])
-    #         print("Message_id:", message["message"]["message_id"])
-    #         print("Chat_id:", message["message"]["chat"]["id"])
-    #         print("Date:", message["message"]["date"])
-    #         print("Text:", message["message"]["text"])
-    #         print("is_bot:", message["message"]["from"]["is_bot"])
-    #         line = int(message["message"]['chat']["id"])
-    #         print(line)
-    #         print('---------------------------------')
-    # except:
-    #     print('Error')
-    #
-    # try:
-    #     if message["callback_query"] is not None:
-    #         # For callbacks
-    #         print("Update_id:", message["update_id"])
-    #         print("Callback_query:", message["callback_query"]['message']["message_id"])
-    #         print("From:", message["callback_query"]['from']["id"])
-    #         print("Callback_data:", message["callback_query"]["data"])
-    #         print("Date:", message["callback_query"]['from']["date"])
-    #         print("Text of the callback Initiating message:", message["callback_query"]['from']["text"])
-    #         print("is_bot:", message["message"]["from"]["is_bot"])
-    #         line = int(message["callback_query"]['from']["id"])
-    #         print(line)
-    #         print('---------------------------------')
-    # except:
-    #     print('Error')
-    #
-    # if line == 0:
-    #     breakpoint()
-    #
-    # # Language choice
-    # markup = telebot.types.InlineKeyboardMarkup()
-    # button_1 = types.InlineKeyboardButton("Fuck You!", callback_data="1")
-    # button_2 = types.InlineKeyboardButton("No, Fuck You", callback_data="2")
-    #
-    # markup.add(button_1, button_2)
-    # await bot.send_message(line, "Welcome to HealthC bot. Choose system language: ",
-    #                        reply_markup=markup)
-    # return "ok"
+        print('Callback data:', message['callback_query']['data'])
+        # Handling
 
 
 @app.get("/check/{name}")
@@ -110,12 +88,10 @@ async def say_hello(name: str):
 def set_webhook():
     url = f'https://api.telegram.org/bot{token}/setWebhook'
     webhook_url = 'https://test2.western-gate.online/message'
-
     response = requests.post(url, json={'url': webhook_url})
-
     print(response.text)
 
 
 if __name__ == "__main__":
     bot.set_webhook()
-    uvicorn.run(app, host='localhost', port=9012)
+    uvicorn.run(app, host='127.0.0.1', port=13412)
